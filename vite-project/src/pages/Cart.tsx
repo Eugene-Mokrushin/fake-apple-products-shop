feaf3906daad52af321e3a4b42a2e46f17358492
Eugene Mokrushin <eugeniymokrushin@gmail.com>
2023-01-30 18:57:07 +0100

    finished cart

--- vite-project/src/pages/Cart.tsx
+++ vite-project/src/pages/Cart.tsx
@@ -35,7 +35,9 @@ export function Cart() {
     const goToCatalogBtn = cart_data.empty.goTo[lang as keyof typeof cart_data.empty.goTo]
     const deliveryTitle = cart_data.delivery.title[lang as keyof typeof cart_data.delivery.title]
     const deliveryOptionCourier = cart_data.delivery.delivery_uptions.courier[lang as keyof typeof cart_data.delivery.delivery_uptions.courier]
+    const deliveryOptionCourierChecker = cart_data.delivery.delivery_uptions.courier.en
     const deliveryOptionPickup = cart_data.delivery.delivery_uptions.selfPickUp[lang as keyof typeof cart_data.delivery.delivery_uptions.selfPickUp]
+    const deliveryOptionPickupChecker = cart_data.delivery.delivery_uptions.selfPickUp.en
     const totalTitle = cart_data.total.title[lang as keyof typeof cart_data.total.title]
     const totalCta = cart_data.total.cta[lang as keyof typeof cart_data.total.cta]
     const [currencyMultiplier, setCurrencyMultiplier] = useState<number>(1)
@@ -49,7 +51,7 @@ export function Cart() {
 
     const calculatedTotal = (+cartItems.reduce((acc, curentValue) => {
         return acc + curentValue.quantity * ((allgoods_data[curentValue.id as keyof {}] as GoodData).price !== '' ? +(allgoods_data[curentValue.id as keyof {}] as GoodData).price.split('$')[1] : 10)
-    }, 0) * currencyMultiplier + (deliveryMethod === deliveryOptionCourier ? lang === "en" ? 10.99 : 10.99 * currencyMultiplier : 0)).toFixed(2)
+    }, 0) * currencyMultiplier + (deliveryMethod === deliveryOptionCourierChecker ? lang === "en" ? 10.99 : 10.99 * currencyMultiplier : 0)).toFixed(2)
 
 
     useEffect(() => {
@@ -66,7 +68,7 @@ export function Cart() {
             setCurrencyMultiplier(multiplier)
         }
         changePriceCurrency()
-        if (deliveryMethod === "") pickDeliveryMethod(deliveryOptionCourier)
+        if (deliveryMethod === "") pickDeliveryMethod(deliveryOptionCourierChecker)
     }, [])
 
     return (
@@ -91,37 +93,37 @@ export function Cart() {
                             <div className={classes.delivery}>
                                 <div className={classes.titleAndPrice}>
                                     <div className={classes.deliveryTitle}>{deliveryTitle}</div>
-                                    <div className={classes.price}>{deliveryMethod === deliveryOptionPickup ? lang === "en" ? "$0.00" : "0 ₽" : lang === "en" ? "$10.99" : `${(10.99 * currencyMultiplier).toFixed(2)} ₽`}</div>
+                                    <div className={classes.price}>{deliveryMethod === deliveryOptionPickupChecker ? lang === "en" ? "$0.00" : "0 ₽" : lang === "en" ? "$10.99" : `${(10.99 * currencyMultiplier).toFixed(2)} ₽`}</div>
                                 </div>
-                                {deliveryMethod === deliveryOptionPickup &&
+                                {deliveryMethod === deliveryOptionPickupChecker &&
                                     <a href="https://www.google.ru/maps/place/Louvre+Museum/@48.8595951,2.3374212,17z/data=!4m5!3m4!1s0x47e671d877937b0f:0xb975fcfa192f84d4!8m2!3d48.8606111!4d2.337644" target="_blank">
                                         <img src="../imgs/map.jpg" alt="map" className={classes.map} />
                                     </a>
                                 }
                                 <div className={classes.deliveryMethodsWrapper}>
                                     <div className={classes.selected} onClick={() => setDeliveryMenuState(prev => !prev)}>
-                                        {deliveryMethod === deliveryOptionCourier ?
+                                        {deliveryMethod === deliveryOptionCourierChecker ?
                                             <svg className={classes.methodThumb} width="20" height="13" viewBox="0 0 20 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M7.29708 10.5584C7.19175 11.236 6.82327 11.856 6.25932 12.3045C5.69536 12.7531 4.9738 13 4.22718 13C3.48055 13 2.75899 12.7531 2.19504 12.3045C1.63108 11.856 1.2626 11.236 1.15728 10.5584H0.240295V0.812182C0.240295 0.596778 0.333639 0.390196 0.499791 0.237883C0.665944 0.0855691 0.891294 0 1.12627 0H13.5299C13.7649 0 13.9902 0.0855691 14.1564 0.237883C14.3225 0.390196 14.4159 0.596778 14.4159 0.812182V2.43655H17.0738L19.7317 5.73076V10.5584H17.9288C17.8234 11.236 17.455 11.856 16.891 12.3045C16.3271 12.7531 15.6055 13 14.8589 13C14.1122 13 13.3907 12.7531 12.8267 12.3045C12.2628 11.856 11.8943 11.236 11.789 10.5584H7.29708ZM12.6439 1.62436H2.01224V8.16243C2.36182 7.83528 2.79243 7.59039 3.26745 7.44862C3.74247 7.30684 4.24782 7.27236 4.74049 7.34813C5.23317 7.42389 5.69858 7.60766 6.09712 7.88377C6.49567 8.15989 6.81554 8.52019 7.02951 8.934H12.0565C12.2054 8.6473 12.4047 8.38659 12.6439 8.16243V1.62436ZM14.4159 6.49746H17.9598V6.26599L16.1807 4.06091H14.4159V6.49746ZM14.8589 11.3705C15.2114 11.3705 15.5496 11.2422 15.7989 11.0136C16.0482 10.7851 16.1883 10.4751 16.1883 10.1519C16.1883 9.82866 16.0482 9.51868 15.7989 9.29013C15.5496 9.06159 15.2114 8.93319 14.8589 8.93319C14.5063 8.93319 14.1681 9.06159 13.9188 9.29013C13.6695 9.51868 13.5295 9.82866 13.5295 10.1519C13.5295 10.4751 13.6695 10.7851 13.9188 11.0136C14.1681 11.2422 14.5063 11.3705 14.8589 11.3705ZM5.55614 10.1523C5.55614 9.99229 5.52176 9.83387 5.45498 9.68606C5.38819 9.53825 5.2903 9.40395 5.1669 9.29083C5.04349 9.1777 4.89699 9.08796 4.73575 9.02674C4.57451 8.96552 4.4017 8.934 4.22718 8.934C4.05266 8.934 3.87984 8.96552 3.71861 9.02674C3.55737 9.08796 3.41087 9.1777 3.28746 9.29083C3.16406 9.40395 3.06616 9.53825 2.99938 9.68606C2.93259 9.83387 2.89822 9.99229 2.89822 10.1523C2.89822 10.4754 3.03823 10.7853 3.28746 11.0137C3.53669 11.2422 3.87472 11.3705 4.22718 11.3705C4.57964 11.3705 4.91767 11.2422 5.1669 11.0137C5.41612 10.7853 5.55614 10.4754 5.55614 10.1523Z" fill="#101010" />
                                             </svg>
                                             :
                                             <img className={classes.methodThumb} src="./imgs/walkinkingman.svg" alt="Pickup" />
                                         }
-                                        <div className={classes.nameOption}>{deliveryMethod}</div>
+                                        <div className={classes.nameOption}>{deliveryMethod === deliveryOptionCourierChecker ? deliveryOptionCourier : deliveryOptionPickup}</div>
                                         <svg className={`${classes.tick} ${!deliveryMenuState ? classes.tickDown : classes.tickUp}`} width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M7.17451 8L0.677368 1.99965L2.8441 0L7.17451 4.00071L11.5049 0L13.6717 1.99965L7.17451 8Z" fill="#101010" />
                                         </svg>
                                     </div>
                                     <div className={`${classes.otherOptions} ${deliveryMenuState ? classes.otherOprionsOpened : classes.otherOprionsClosed}`}>
-                                        <div className={classes.option} onClick={() => { (deliveryMethod === deliveryOptionCourier ? pickDeliveryMethod(deliveryOptionPickup) : pickDeliveryMethod(deliveryOptionCourier)); setDeliveryMenuState(false) }}>
-                                            {deliveryMethod === deliveryOptionPickup ?
+                                        <div className={classes.option} onClick={() => { (deliveryMethod === deliveryOptionPickupChecker ? pickDeliveryMethod(deliveryOptionCourierChecker) : pickDeliveryMethod(deliveryOptionPickupChecker)); setDeliveryMenuState(false) }}>
+                                            {deliveryMethod === deliveryOptionPickupChecker ?
                                                 <svg className={classes.methodThumb} width="20" height="13" viewBox="0 0 20 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M7.29708 10.5584C7.19175 11.236 6.82327 11.856 6.25932 12.3045C5.69536 12.7531 4.9738 13 4.22718 13C3.48055 13 2.75899 12.7531 2.19504 12.3045C1.63108 11.856 1.2626 11.236 1.15728 10.5584H0.240295V0.812182C0.240295 0.596778 0.333639 0.390196 0.499791 0.237883C0.665944 0.0855691 0.891294 0 1.12627 0H13.5299C13.7649 0 13.9902 0.0855691 14.1564 0.237883C14.3225 0.390196 14.4159 0.596778 14.4159 0.812182V2.43655H17.0738L19.7317 5.73076V10.5584H17.9288C17.8234 11.236 17.455 11.856 16.891 12.3045C16.3271 12.7531 15.6055 13 14.8589 13C14.1122 13 13.3907 12.7531 12.8267 12.3045C12.2628 11.856 11.8943 11.236 11.789 10.5584H7.29708ZM12.6439 1.62436H2.01224V8.16243C2.36182 7.83528 2.79243 7.59039 3.26745 7.44862C3.74247 7.30684 4.24782 7.27236 4.74049 7.34813C5.23317 7.42389 5.69858 7.60766 6.09712 7.88377C6.49567 8.15989 6.81554 8.52019 7.02951 8.934H12.0565C12.2054 8.6473 12.4047 8.38659 12.6439 8.16243V1.62436ZM14.4159 6.49746H17.9598V6.26599L16.1807 4.06091H14.4159V6.49746ZM14.8589 11.3705C15.2114 11.3705 15.5496 11.2422 15.7989 11.0136C16.0482 10.7851 16.1883 10.4751 16.1883 10.1519C16.1883 9.82866 16.0482 9.51868 15.7989 9.29013C15.5496 9.06159 15.2114 8.93319 14.8589 8.93319C14.5063 8.93319 14.1681 9.06159 13.9188 9.29013C13.6695 9.51868 13.5295 9.82866 13.5295 10.1519C13.5295 10.4751 13.6695 10.7851 13.9188 11.0136C14.1681 11.2422 14.5063 11.3705 14.8589 11.3705ZM5.55614 10.1523C5.55614 9.99229 5.52176 9.83387 5.45498 9.68606C5.38819 9.53825 5.2903 9.40395 5.1669 9.29083C5.04349 9.1777 4.89699 9.08796 4.73575 9.02674C4.57451 8.96552 4.4017 8.934 4.22718 8.934C4.05266 8.934 3.87984 8.96552 3.71861 9.02674C3.55737 9.08796 3.41087 9.1777 3.28746 9.29083C3.16406 9.40395 3.06616 9.53825 2.99938 9.68606C2.93259 9.83387 2.89822 9.99229 2.89822 10.1523C2.89822 10.4754 3.03823 10.7853 3.28746 11.0137C3.53669 11.2422 3.87472 11.3705 4.22718 11.3705C4.57964 11.3705 4.91767 11.2422 5.1669 11.0137C5.41612 10.7853 5.55614 10.4754 5.55614 10.1523Z" fill="#101010" />
                                                 </svg>
                                                 :
                                                 <img className={classes.methodThumb} src="./imgs/walkinkingman.svg" alt="Pickup" />
                                             }
-                                            <div className={classes.nameOption}>{deliveryMethod === deliveryOptionCourier ? deliveryOptionPickup : deliveryOptionCourier}</div>
+                                            <div className={classes.nameOption}>{deliveryMethod === deliveryOptionCourierChecker ? deliveryOptionPickup : deliveryOptionCourier}</div>
                                         </div>
                                     </div>
                                 </div>
@@ -133,7 +135,7 @@ export function Cart() {
                             <div className={classes.title}>{totalTitle.toUpperCase()}</div>
                             <div className={classes.price}>{lang === "en" ? `$${calculatedTotal}` : `${calculatedTotal} ₽`}</div>
                         </div>
-                        <Link to={`/checkout`} onClick={() => {setSection(lang === "en" ? "Checkout" : "Оформление заказа")}} style={{textDecoration: "none"}}>
+                        <Link to={`/checkout`} onClick={() => { setSection(lang === "en" ? "Checkout" : "Оформление заказа") }} style={{ textDecoration: "none" }}>
                             <div className={classes.btn}>{totalCta}</div>
                         </Link>
                     </div>
--- vite-project/src/pages/Checkout.tsx
+++ vite-project/src/pages/Checkout.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import classes from '../../scss/Checkout.module.scss'
+import checkout_data from '../data/Checkout.json';
+import goods_data from '../data/individual_good.json';
+import cart_data from '../data/Cart.json'
 import { CheckoutDelivery } from '../components/CheckoutDelivery'
 import { CheckoutGoods } from '../components/CheckoutGoods'
 import { CheckoutPayment } from '../components/CheckoutPayment'
@@ -11,6 +14,25 @@ export function Checkout() {
     const { lang } = useMobileAndLang()
     const { deliveryMethod, cartItems } = useShoppingCart()
     const [currencyMultiplier, setCurrencyMultiplier] = useState<number>(1)
+    const [ribbon, setRibbon] = useState<JSX.Element[]>()
+    const [totalPrice, setTotalPrice] = useState<number>()
+    const [discount, setDiscount] = useState<number>(0)
+    const totalRef = useRef(null)
+    const total = checkout_data.total[lang as keyof typeof checkout_data.total]
+    const placeOrder = checkout_data.ctaBtn[lang as keyof typeof checkout_data.ctaBtn]
+    const deliveryOptionPickupChecker = cart_data.delivery.delivery_uptions.selfPickUp.en
+    const allPromocodes = cart_data.allPromocodes
+
+    type GoodData = {
+        "title": string,
+        "price": string,
+        "rating": string,
+        "feature": string[],
+        "specific_name": string[],
+        "specific_value": string[],
+        "description": string,
+        "images_url": string[]
+    }
 
     useEffect(() => {
         async function changePriceCurrency() {
@@ -26,13 +48,61 @@ export function Checkout() {
             setCurrencyMultiplier(multiplier)
         }
         changePriceCurrency()
+
     }, [])
 
+    useEffect(() => {
+        if (totalRef.current !== null) {
+            const widthTotal = (totalRef.current as HTMLElement).getBoundingClientRect().width;
+            let ribbon = []
+            for (let index = 0; index < widthTotal / 10; index++) {
+                ribbon.push(<div key={crypto.randomUUID()} className={classes.ribbonTriangle}></div>)
+            }
+            setRibbon(ribbon)
+        }
+        let total = 0
+        for (let index = 0; index < cartItems.length; index++) {
+            const product_data: GoodData = goods_data[cartItems[index].id as keyof typeof goods_data]
+            total += (+product_data.price.split('$')[1] * cartItems[index].quantity) * currencyMultiplier
+        }
+        total += deliveryMethod === deliveryOptionPickupChecker ? 0 * currencyMultiplier : 10.99 * currencyMultiplier
+        setTotalPrice(+total.toFixed(2))
+    }, [currencyMultiplier])
+
+    function handlePromo(promocode: string) {
+        if (allPromocodes.includes(promocode)) {
+            setDiscount(cart_data.promocodes[promocode as keyof typeof cart_data.promocodes])
+        } else {
+            setDiscount(0)
+        }
+    }
+
     return (
-        <div className={classes.checkoutWrapper}>
-            <CheckoutGoods lang={lang} cartItems={cartItems} currencyMultiplier={currencyMultiplier} />
-            <CheckoutDelivery lang={lang} deliveryMethod={deliveryMethod} currencyMultiplier={currencyMultiplier} />
-            <CheckoutPayment lang={lang} />
+        <div className={classes.container}>
+            <div className={classes.checkoutWrapper}>
+                <div className={classes.blocks}>
+                    <CheckoutGoods lang={lang} cartItems={cartItems} currencyMultiplier={currencyMultiplier} />
+                    <CheckoutDelivery lang={lang} deliveryMethod={deliveryMethod} currencyMultiplier={currencyMultiplier} />
+                    <CheckoutPayment lang={lang} handlePromo={handlePromo} />
+                </div>
+            </div>
+            <div className={classes.totalWrapperPlaceOrder}>
+                <div className={classes.total}>
+                    <div className={classes.title}>{total.toUpperCase()}</div>
+                    <div className={classes.priceAndDiscaout}>
+                        <div className={`${classes.sum} ${discount !== 0 ? classes.oldPrice : ""}`}>{lang === "en" ? "$" + totalPrice : totalPrice + " ₽"}</div>
+                        {discount !== 0 && totalPrice &&
+                            <div className={classes.newPrice}>
+                                {lang === "en" ? "$" + (totalPrice * (100 - discount) / 100).toFixed(2) : (totalPrice * (100 - discount) / 100).toFixed(2) + " ₽"}
+                            </div>
+                        }
+                    </div>
+                </div>
+                <div className={classes.placeOrderWrapper} ref={totalRef}>
+                    {ribbon}
+                    <div className={classes.button}>{placeOrder}</div>
+                </div>
+            </div>
         </div>
     )
 }
